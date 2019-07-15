1ba6f20cc69ab1bd208690f63df11f51967b9969
Marcin Stąpor <52878339+marcinstapor@users.noreply.github.com>
2019-07-15 23:29:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,12 @@
 #include <iostream>
-#include <cmath>
-#include <algorithm>
-#include <set>
-#include <vector>
-#include <stack>
 
 using namespace std;
 
-
-
-int main() {
-
-   ios_base::sync_with_stdio(0);
-
-     char a, b, c;
-    while(!cin.eof())
-    {
-      cin >> a >> b;
-	  if(cin.fail()) break;
-	  c=(a-'0')*10+b-'0';
-	 // if(c<' ') c+=100;
-	  cout << c;
-	}
-
+int main()
+{
+   int a, b, n;
+   cin >> a >> b >> n;
+   cout << (4*a*b-n) << endl;
+ 
    return 0;
 }
-
-
